1919c090585e7d733d187e689f55d5f88fb838eb
Mititelu Vlad <vlad-george.mititelu@s.unibuc.ro>
2026-05-05 03:22:28 +0300

    fix commit

---
+++ Hubs/ActionHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication1.Hubs
+{
+    // Hub-ul este punctul central prin care trec mesajele Real-time
+    public class AuctionHub : Hub
+    {
+        // Nu este nevoie de metode aici pentru notificările de tip "Outbid", 
+        // deoarece serverul (Controller-ul) va trimite mesaje direct către clienți.
+    }
+}
